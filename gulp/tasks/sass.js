d395d9adb2423ab5270291057d8feca231e5be69
VitalyTkhir <vitslikus865@gmail.com>
2019-08-23 04:18:19 +0400

    23.08.2019

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -4,7 +4,8 @@ module.exports = function() {
 		return $.gulp.src($.path.src.sass, {since: $.gulp.lastRun('sass:development')})
 		.pipe($.sourcemaps.init())
 		.pipe($.aliases({
-			'~modules': 'node_modules'
+			'~': 'node_modules/',
+			'@': 'src/sass/helpers/'
 		}))
 		.pipe($.sass({outputStyle: 'compact'}).on('error', $.sass.logError))
 		.pipe($.autoprefixer())
@@ -16,12 +17,13 @@ module.exports = function() {
 	$.gulp.task('sass:production', function () {
 		return $.gulp.src($.path.src.sass)
 		.pipe($.aliases({
-			'~modules': 'node_modules'
+			'~': 'node_modules/',
+			'@': 'src/sass/helpers/'
 		}))
 		.pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
 		.pipe($.autoprefixer())
 		.pipe($.stripCss())
 		.pipe($.gulp.dest($.path.build.css))
 	});
-	
+
 };
